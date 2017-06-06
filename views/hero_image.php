9a19bf3adc2b59c3be5bbd02f5322ada5100c1d2
austinmitch <mitchellaustinj@gmail.com>
2017-06-06 07:27:45 -0400

    started skeleton on of hero image block

--- views/hero_image.php
+++ views/hero_image.php
@@ -1,3 +1,8 @@
 <div class="hero_image">
-  <p>hero image</p>
+  <div class="banner">
+    <img src="" alt="dummy image">
+  </div>
+  <div class="call-to-action">
+    <h3>dummy image for a dummy webpage</h3>
+  </div>
 </div>
